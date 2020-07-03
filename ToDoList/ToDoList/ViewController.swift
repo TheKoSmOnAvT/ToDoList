8713d2bd40d421bb6babd503aa38af2e5938b0c1
TheKoSmOnAvT <ni_popov@mail.ru>
2020-07-03 19:13:18 +0500

    init rep and views

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Никита Попов on 03.07.2020.
+//  Copyright © 2020 Никита Попов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
